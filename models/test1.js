5d3746c9503ea5039e4446853cf2ab368d743ac6
Anton Pretorius <Ajax401@users.noreply.github.com>
2019-01-31 18:06:40 +0200

    Add files via upload

--- models/test1.js
+++ models/test1.js
@@ -1,20 +1,19 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-
-const mySchema = new Schema({
-  count:[Number],
-  username:String,
+const mySchema = new Schema ({
   _id:String,
-  description:String,
-  duration:[Number],
-  date:[Schema.Types.Mixed],
-  description:[String] 
-})
+  username: String,
+  data: [{
+	_id:false,  
+    description : String,
+    duration: Number,
+    date :{type:Date}
+  }]
+});
 
 const ExampleClass = mongoose.model('array',mySchema);
 
-
 module.exports = ExampleClass;
-
